da2b56339f077be3d85308af560a0f59cecfb9dd
kitweman <kitweman@gmail.com>
2023-12-03 08:27:51 +0300

    new form drafted

--- pharmacy-app/src/App.js
+++ pharmacy-app/src/App.js
@@ -2,10 +2,14 @@
 
 import React from 'react';
 import InventoryComponent from './components/InventoryComponent';
+import NewMedicine from './components/form/AddMedicineForm';
+import SaleForm from './components/form/SaleForm';
 
 function App() {
   return (
     <div className="App">
+      <NewMedicine/>
+      <SaleForm/>
       <InventoryComponent />
     </div>
   );
