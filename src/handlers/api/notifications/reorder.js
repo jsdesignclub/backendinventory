602be0b3c8d55b0c0a7e25f9a2cd3b1462d4c0f2
KilonzoHussein <chusechusechuse09@gmail.com>
2024-04-21 09:47:44 +0300

    Merge branch 'master' of https://github.com/dnamkaa/Pharmacy_management

--- src/handlers/api/notifications/reorder.js
+++ src/handlers/api/notifications/reorder.js
@@ -1,23 +1,23 @@
-'use strict';
+// 'use strict';
 
-const inventoryController = require("../../../controllers/inventoryController");
+// const inventoryController = require("../../../controllers/inventoryController");
 
-/*
- * Operations on /api/notifications/reorder
- */
-module.exports = {
-    /**
-     * summary: Notify reorder points
-     * description: Notify the front end about drugs reaching reorder points.
-     * parameters: body
-     * produces: 
-     * responses: 201
-     */
-    post: function (req, res, next) {
-        /*
-         * Get the data for response 201
-         * For response `default` status 200 is used.
-         */
-       return inventoryController.addnewReorder(req, res, next);
-    }
-};
+// /**
+//  * Operations on /api/notifications/reorder
+//  */
+// module.exports = {
+//     /**
+//      * summary: Notify reorder points
+//      * description: Notify the front end about drugs reaching reorder points.
+//      * parameters: body
+//      * produces: 
+//      * responses: 201
+//      */
+//     post: function (req, res, next) {
+//         /**
+//          * Get the data for response 201
+//          * For response `default` status 200 is used.
+//          */
+//        return inventoryController.addnewReorder(req, res, next);
+//     }
+// };
